5d39aedc8fc3268546a74865c05e993d3beceaad
jumagingaa <jumagingaa@uteq.edu.ec>
2026-07-13 10:05:39 -0500

    Agregar servicio ProductoService

---
+++ taller-jpa/src/main/java/ec/edu/uteq/tallerjpa/service/ProductoService.java
@@ -0,0 +1,29 @@
+package ec.edu.uteq.tallerjpa.service;
+
+import ec.edu.uteq.tallerjpa.entity.Producto;
+import ec.edu.uteq.tallerjpa.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService {
+
+    private final ProductoRepository repository;
+
+    public ProductoService(ProductoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Producto> listar() {
+        return repository.findAll();
+    }
+
+    public Producto guardar(Producto producto) {
+        return repository.save(producto);
+    }
+
+    public void eliminar(Long id) {
+        repository.deleteById(id);
+    }
+}
