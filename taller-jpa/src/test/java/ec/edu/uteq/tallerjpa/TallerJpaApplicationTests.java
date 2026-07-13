22634ef002e4c861bd5da65297c1078839a59386
jumagingaa <jumagingaa@uteq.edu.ec>
2026-07-13 09:53:47 -0500

    Crear proyecto Spring Data JPA

---
+++ taller-jpa/src/test/java/ec/edu/uteq/tallerjpa/TallerJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uteq.tallerjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
